9b8b13731cf48ed0830201ea8f2ce76269bd4db7
DSM2 <fatec-dsm2@academico.intra>
2026-09-16 20:27:37 -0300

    rota de ligas (rota3)

--- atv01/index.js
+++ atv01/index.js
@@ -44,6 +44,47 @@ app.get("/clubes", (req, res) =>{
 
     res.render('clubes', {clubes});
 });
+app.get("/ligas", (req, res) => {
+    const ligas = [
+        {
+            nome: "Brasileirão Série A",
+            pais: "Brasil",
+            maiorCampeao: "Palmeiras",
+            confederacao: "CONMEBOL",
+            imagem: "/images/brasileirao.png"
+        },
+        {
+            nome: "Premier League",
+            pais: "Inglaterra",
+            maiorCampeao: "Manchester United",
+            confederacao: "UEFA",
+            imagem: "/images/premierleague.png"
+        },
+        {
+            nome: "Libertadores",
+            pais: "América do Sul",
+            maiorCampeao: "Independiente",
+            confederacao: "CONMEBOL",
+            imagem: "/images/libertadores.png"
+        },
+        {
+            nome: "Serie A TIM",
+            pais: "Itália",
+            maiorCampeao: "Juventus",
+            confederacao: "UEFA",
+            imagem: "/images/seriea.png"
+        },
+        {
+            nome: "La Liga",
+            pais: "Espanha",
+            maiorCampeao: "Real Madrid",
+            confederacao: "UEFA",
+            imagem: "/images/laliga.png"
+        }
+    ];
+
+    res.render('ligas', { ligas });
+});
 
 // Método do Express para iniciar o servidor back-end
 // app.listen()
