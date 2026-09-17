94b3e5b2a58991d61eb7519b31f6763191904250
mario-tdias <mariotakahashi69@gmail.com>
2026-09-17 13:07:13 -0300

    rota jogadores (rota4)

--- atv01/index.js
+++ atv01/index.js
@@ -86,6 +86,38 @@ app.get("/ligas", (req, res) => {
     res.render('ligas', { ligas });
 });
 
+app.get("/jogadores", (req, res) => {
+
+   const jogadores = [
+        {
+            nome: "Lucas Moura",
+            posicao: "Atacante / Meia",
+            clube: "São Paulo",
+            nacionalidade: "Brasil",
+            numero: 7,
+            imagem: "/images/lucasmoura.png"
+        },
+        {
+            nome: "Paulo Dybala",
+            posicao: "Meia / Atacante",
+            clube: "AS Roma",
+            nacionalidade: "Argentina",
+            numero: 21,
+            imagem: "/images/dybala.png"
+        },
+        {
+            nome: "Carlos Gárces",
+            posicao: "Meia",
+            clube: "Cienciano",
+            nacionalidade: "Equador",
+            numero: 21,
+            imagem: "/images/carlosgarces.png"
+        }
+    ];
+
+    res.render('jogadores', {jogadores});
+})
+
 // Método do Express para iniciar o servidor back-end
 // app.listen()
 const port = 8080;
